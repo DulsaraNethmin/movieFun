8a7d65411287365081f0686f285470f165c79879
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-05-23 01:06:47 +0530

    fix-3

--- server.js
+++ server.js
@@ -16,21 +16,20 @@ app.use(cors());
 
 
 //API routes
-mongoose.connect("mongodb+srv://movieUser:movieUser123@nethmincluster99.5wvxn.mongodb.net/Moviedb?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
         .then(()=>{console.log("server connected")})
         .catch((err)=>{console.log("an error happened")});
 
 app.use('/',movie);
 
 
-if(process.env.NODE_ENV==='production')
-{
-    app.use(express.static('/client/build'));
-    app.get("*",(req,res)=>
-    {
-        res.sendFile(path.resolve(__dirname,'client/build/index.html'));
-    })
-}
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
 
